d224ae03bfbef45d5012786d03c9cc74743a04a5
Sabrina <pereira.tsabrina@gmail.com>
2021-10-15 10:22:58 -0300

    focus on full name added

--- Javascript/subs-week-5.js
+++ Javascript/subs-week-5.js
@@ -1,5 +1,5 @@
 //Full Name Validation
-
+//Blur
 
 let inputName = document.querySelector("#full-name");
 inputName.addEventListener("blur",fullNameValidation);
@@ -7,10 +7,19 @@ inputName.addEventListener("blur",fullNameValidation);
 function fullNameValidation(e) {
     const subsForm = e.target.value;
     if (subsForm==""){
-        alert("Full name is required");
+        document.getElementsByClassName("error-txt")[0].innerHTML = "Full name is required";
     }else if(subsForm.length < 6){
-        alert("It's not a valid name it has less than 6 characters");
+        document.getElementsByClassName("error-txt")[0].innerHTML = "It's not a valid name it has less than 6 characters";
     }else if(subsForm.indexOf(" ")==(-1)){
-        alert("There's no space between the name and last name");
+        document.getElementsByClassName("error-txt")[0].innerHTML = "Check if you wrote name and last name or if it has a space between those two";
+    }else{
+        document.getElementsByClassName("error-txt")[0].innerHTML = "";
     }
+}
+
+//Focus
+inputName.addEventListener("focus",focusValidation);
+
+function focusValidation(e) {
+    e.target.parentNode.children[2].innerHTML = "";
 }
